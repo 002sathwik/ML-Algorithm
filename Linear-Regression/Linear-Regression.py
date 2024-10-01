b39d1596432d2316b88a2188b47e2ab752439f4d
Sathwik <hsathwik002@gmail.com>
2024-10-01 23:16:47 +0530

    Feath:Visualising Trained/Tested-> plot

--- Linear-Regression/Linear-Regression.py
+++ Linear-Regression/Linear-Regression.py
@@ -20,3 +20,22 @@ x_train,x_test,y_train,y_test = train_test_split(x,y, test_size=1/3, random_stat
 from sklearn.linear_model import LinearRegression
 reg = LinearRegression()
 reg.fit(x_train,y_train)
+y_pred = reg.predict(x_test)
+
+#train Data
+plt.scatter(x_train, y_train, color = "red")
+plt.plot(x_train , reg.predict(x_train), color ="blue")
+plt.title('salary vs Experience')
+plt.xlabel('exp')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+# test Data
+plt.scatter(x_test, y_test, color = "red")
+plt.plot(x_train , reg.predict(x_train), color ="blue")
+plt.title('salary vs Experience')
+plt.xlabel('exp')
+plt.ylabel('Salary')
+plt.show()
