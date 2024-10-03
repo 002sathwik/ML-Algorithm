42b113793597ac5107485ec37df70504de2232ac
Sathwik <hsathwik002@gmail.com>
2024-10-03 08:23:31 +0530

    Feath:ModelTraining/RestultPRediction

--- Support-Vector -Machine/support-Vector_machine.py
+++ Support-Vector -Machine/support-Vector_machine.py
@@ -19,7 +19,22 @@ from sklearn.preprocessing import StandardScaler
 sx=StandardScaler()
 sy=StandardScaler()
 X=sx.fit_transform(x)
-y=sy.fit_transform(y)
+Y=sy.fit_transform(y)
 
-print(x)
-print(y)
+
+
+
+from sklearn.svm import SVR
+reg=SVR(kernel='rbf')
+reg.fit(X,Y)
+
+# Predict the value for X=6.5
+predicted_value = reg.predict(sx.transform([[6.5]]))
+
+# Reshape the predicted value to 2D (n_samples, 1) before applying inverse_transform
+predicted_value_reshaped = predicted_value.reshape(-1, 1)
+
+# Apply inverse transformation to get the predicted value back in the original scale
+original_scale_value = sy.inverse_transform(predicted_value_reshaped)
+
+print(original_scale_value)
