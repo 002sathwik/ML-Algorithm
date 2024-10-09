31c2f1b93003d50cdd8b8f3bf9e127d6be58691c
Sathwik <hsathwik002@gmail.com>
2024-10-09 22:26:53 +0530

    Feath:modeltraining/Cluster->plot

--- K-Means/Kmeans.py
+++ K-Means/Kmeans.py
@@ -2,22 +2,50 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 
+# Load the dataset
 dataset = pd.read_csv("Mall_Customers.csv") 
 
-x=dataset.iloc[:,3:].values
+# Selecting the relevant columns
+x = dataset.iloc[:, 3:].values
 
+# KMeans clustering
 from sklearn.cluster import KMeans
 
+# List to store WCSS values
 ws = [] 
 
+# Elbow method to find the optimal number of clusters
 for i in range(1, 11):
     kmeans = KMeans(n_clusters=i, init="k-means++", random_state=42)
     kmeans.fit(x)  
     ws.append(kmeans.inertia_) 
 
-    
-plt.plot(range(1,11),ws)
+# Plotting the elbow method result
+plt.plot(range(1, 11), ws)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
+
+# Model training with 5 clusters
+kmeans = KMeans(n_clusters=5, init="k-means++", random_state=42)
+y_kmeans = kmeans.fit_predict(x)
+
+# Plotting the clusters
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s=100, c='red', label='Cluster 1')
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s=100, c='green', label='Cluster 2')
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s=100, c='blue', label='Cluster 3')
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s=100, c='cyan', label='Cluster 4')
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s=100, c='magenta', label='Cluster 5')
+
+# Correcting the centroids reference
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=300, c='yellow', label='Centroids')
+
+# Final plot settings
+plt.title('Clusters of Customers')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
+
+
