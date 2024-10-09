21e6b51c43e1113e9848eaab1b70dcbfbfda88d8
Sathwik <hsathwik002@gmail.com>
2024-10-09 21:14:35 +0530

    Feath:Kmeans-clustering-file

--- Support-Vector -Machine/support-Vector_machine.py
+++ Support-Vector -Machine/support-Vector_machine.py
@@ -1,45 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Oct  3 08:03:06 2024
-
-@author: USER
-"""
-
-import pandas as pd 
-import numpy as np 
-import matplotlib.pyplot as plt 
-
-data= pd.read_csv("Position_Salaries.csv")
-x=data.iloc[:,1:-1].values
-y=data.iloc[:,-1].values
-y=y.reshape(len(y),1)
+# Support Vector Regression (SVR)
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
 print(y)
-
+y = y.reshape(len(y),1)
+print(y)
+# Feature Scaling
 from sklearn.preprocessing import StandardScaler
-sx=StandardScaler()
-sy=StandardScaler()
-X=sx.fit_transform(x)
-Y=sy.fit_transform(y)
-
-
-
-
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+print(X)
+print(y)
+# Training the SVR model on the whole dataset
 from sklearn.svm import SVR
-reg=SVR(kernel='rbf')
-reg.fit(X,Y)
-# Predict the value for X=6.5
-predicted_value = reg.predict(sx.transform([[6.5]]))
-# Reshape the predicted value to 2D (n_samples, 1) before applying inverse_transform
-predicted_value_reshaped = predicted_value.reshape(-1, 1)
-# Apply inverse transformation to get the predicted value back in the original scale
-original_scale_value = sy.inverse_transform(predicted_value_reshaped)
-print(original_scale_value)
-
-
-plt.scatter(sx.inverse_transform(X), sy.inverse_transform(Y), color='red')
-plt.plot(sx.inverse_transform(reg.predict(X)),color ='blue')
-plt.title("Svr")
-plt.xlabel('level')
+regressor = SVR(kernel = 'rbf')
+regressor.fit(X, y)
+# Predicting a new result
+sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])))
+# Visualising the SVR results
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
+# Visualising the SVR results (for higher resolution and smoother curve)
+X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
+plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
